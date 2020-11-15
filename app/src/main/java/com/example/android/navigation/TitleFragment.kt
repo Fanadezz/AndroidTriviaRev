45d7e7ef9e965e3a7e4e87686528bc2b77012d21
Antony <vontonnie@gmail.com>
2020-11-15 10:32:02 +0300

    Add OptionsMenu

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,18 +1,44 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val binding = FragmentTitleBinding.inflate(inflater )
+        val binding = FragmentTitleBinding.inflate(inflater)
+
+        //tell android that this fragment has a menu
+        setHasOptionsMenu(true)
+        binding.playButton.setOnClickListener {
+            //Navigation can also create onClickListener
+            //  Navigation.createNavigateOnClickListener()
+            findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            //add menu
+
+        }
         return binding.root
+    }
+
+    //called to inflate the menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    //onOptionsItemSelected called when menu item is selected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        //onNavDestinationSelected passing in the MenuItem and NavController
+        return NavigationUI.onNavDestinationSelected(item, findNavController())
+                || super.onOptionsItemSelected(item)
+
     }
 }
